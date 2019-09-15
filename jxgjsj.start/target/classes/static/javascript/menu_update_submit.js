7969d2385fd95ad7c1a11d5016b81b46b011f629
359442094 <359442094@qq.com>
2019-09-15 20:46:22 +0800

    法克

---
+++ jxgjsj.start/target/classes/static/javascript/menu_update_submit.js
@@ -0,0 +1,38 @@
+// +----------------------------------------------------------------------
+// | 表单提交页面通用表单监听（如有特殊需求可参照此文件写一个单独的js文件，如:sys_config.js）
+// +----------------------------------------------------------------------
+layui.use(['element','form'], function(){
+    var element = layui.element;
+    var form = layui.form;
+    //监听提交
+    form.on('submit(menu_update_submit)', function(){
+        var action ="/menuAdmin/info";//表单提交URL地址
+        var mid = $("input[name='mid']").val(); //表单数据
+        var mname = $("input[name='mname']").val(); //表单数据
+        var mpath = $("input[name='mpath']").val(); //表单数据
+        $.ajax({
+            url:action+"?mid="+mid+"&mname="+mname+"&mpath="+mpath,
+            method:"PUT",
+            success:function(result){
+                if(result == true){
+                    layer.msg('修改成功', {
+                        icon: 1,//提示的样式
+                        time: 3000, //2秒关闭（如果不配置，默认是3秒）//设置后不需要自己写定时关闭了，单位是毫秒
+                        end:function(){
+                            location.href='/serviceAdmin';
+                        }
+                    });
+                }else{
+                    layer.msg('修改失败', {
+                        icon: 2,//提示的样式
+                        time: 3000, //2秒关闭（如果不配置，默认是3秒）//设置后不需要自己写定时关闭了，单位是毫秒
+                        end:function(){
+                            location.href='/serviceAdmin';
+                        }
+                    });
+                }
+            }
+        });
+        return false;//注释掉这行代码后，表单将会以普通方式提交表单，否则以ajax方式提交表单
+    });
+});
